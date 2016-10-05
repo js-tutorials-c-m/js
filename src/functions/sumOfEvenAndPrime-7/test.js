c6cc2ddfdb697b26f9d5b3a62be9a79cc3794c32
karthikalaxmi <karthikapurnima9@gmail.com>
2016-10-05 14:54:27 +0530

    new file

--- src/functions/sumOfEvenAndPrime-7/test.js
+++
@@ -1,9 +0,0 @@
-Code = require('code'),
-expect = Code.expect,
-sumOfEvenAndPrime = require('./script.js');
-describe('An Array of elements',function() {
-	it('should return sum of even and prime numbers',function(){
-		expect(sumOfEvenAndPrime([1,2])).to.be.equal(2);
-		expect(sumOfEvenAndPrime([1,2,4])).to.be.equal(6);
-	});
-});
