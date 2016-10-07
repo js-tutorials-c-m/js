6b5ec09c8f884c96c604bd2ba508f097ee05c963
subbu666 <rayudups666@gmail.com>
2016-10-07 16:28:38 +0530

    square file added

---
+++ src/functions/square-2/square.spec.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+
+Code = require('code'),
+expect = Code.expect,
+AreaOfSquare = require('./square.js');
+describe('Area of square',function() {
+	it('Should calculate the Area of square ',function(){
+		expect(AreaOfSquare(5)).to.be.equal(25);
+		expect(AreaOfSquare(6)).to.be.equal(36);
+		
+	});
+
+});
