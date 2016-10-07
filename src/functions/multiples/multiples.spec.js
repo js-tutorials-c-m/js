becf5490b3caa7a597e7fd09ecc85c26da09595a
sumana0512 <sumana.alaparthi@gmail.com>
2016-10-07 15:01:50 +0530

    add multiples file

---
+++ src/functions/multiples/multiples.spec.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+Code = require('code'),
+expect = Code.expect,
+solution = require('./multiples.js');
+describe('value of 4&5 multiples',function() {
+	it('',function(){
+		expect(solution([24,25,3])).to.be.equal(49);
+		expect(solution([1,2,3,4,5])).to.be.equal(9);
+		expect(solution([1,2,4])).to.be.equal(4);
+		
+	});
+	it('Should return a number',function(){
+		expect(typeof solution([24,25,3])).to.be.equal("number");
+
+	});
+});
