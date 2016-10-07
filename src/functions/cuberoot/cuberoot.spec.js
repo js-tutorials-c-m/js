18bb75bced01d890479df26d54779e23ce47a1f4
satya66 <dsatya66@gmail.com>
2016-10-07 10:55:24 +0530

    added cuberoot file

---
+++ src/functions/cuberoot/cuberoot.spec.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+Code = require('code'),
+expect = Code.expect,
+cuberoot = require('./cuberoot.js');
+
+describe('compute value of cuberoot',function() {
+	
+	it('value of cuberoot',function(){
+		expect(cuberoot(27)).to.be.equal(3);
+		expect(cuberoot(8)).to.be.equal(2);
+		expect(cuberoot(64)).to.be.equal(4);
+	});
+
+});
