a9c6bbe2c2a5e85326a71515d25b4a4211351734
Gauravnag <gauravnag28@gmail.com>
2016-10-07 15:24:43 +0530

    derivative file

--- src/functions/derivative/derivative.js
+++ src/functions/derivative/derivative.js
@@ -1,11 +1,8 @@
 // JavaScript Document
 var derivative= function(x,n){
-	var x=4,
-	    n=3;
-	result=n*Math.pow(x,(n-1));
-	  console.log(result);
-	  return result;
+	
+	  return n*Math.pow(x,(n-1));
+	  
 }
- derivative();
 
 module.exports = derivative;
