1a3fa9a54e5fe24db760081b830c30bde4eab99a
Rajeshkumpati <kumpatirajesh@gmail.com>
2016-10-12 14:51:58 +0530

    factorial of numbers #8

--- src/functions/factorial/fact.spec.js
+++ src/functions/factorial/fact.spec.js
@@ -5,5 +5,8 @@ factorial = require('./fact.js');
 describe('factorial of a number', function() {
 	it('Should calculate the factorial of a number',function(){
 		expect(factorial(4)).to.be.equal(24);
+		expect(factorial(0)).to.be.equal(1);
+		expect(factorial(2)).to.be.equal(2);
+		expect(factorial(5)).to.be.equal(120);
 	});
 });
