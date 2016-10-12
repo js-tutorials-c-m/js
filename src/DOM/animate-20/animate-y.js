72286d492a9e2f03245d6295e70b27a2d8b54add
priyankapithani <priyanka.pithani02@gmail.com>
2016-10-12 15:28:10 +0530

    animate#20

---
+++ src/DOM/animate-20/animate-y.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+
+function animateY(){
+	var element=document.getElementById("animate");
+	var currentPosition=0;
+	var id=setInterval(frame,5);
+	function frame(){
+		if(currentPosition==500){
+			clearInterval(id);
+			}
+		else{
+			currentPosition++;
+			element.style.top=currentPosition+'px';
+			}
+		}
+	}
+	
