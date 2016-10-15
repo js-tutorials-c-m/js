e8aeaeffbb3a838c7dff6833c6f615207d2e0c2e
priyankapithani <priyanka.pithani02@gmail.com>
2016-10-15 10:57:38 +0530

    add animate#20

--- src/DOM/animate-20/animate-y.js
+++ src/DOM/animate-20/animate-y.js
@@ -5,12 +5,13 @@ function animateY(){
 	var currentPosition=0;
 	var id=setInterval(frame,5);
 	function frame(){
-		if(currentPosition==500){
+		if(currentPosition==1500){
 			clearInterval(id);
 			}
 		else{
 			currentPosition++;
 			element.style.top=currentPosition+'px';
+			
 			}
 		}
 	}
