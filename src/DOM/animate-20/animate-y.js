2a1932d52847b63863c1195573a72745c43badad
priyankapithani <priyanka.pithani02@gmail.com>
2016-10-15 14:30:44 +0530

    animate#20 updated

--- src/DOM/animate-20/animate-y.js
+++ src/DOM/animate-20/animate-y.js
@@ -1,18 +1,15 @@
-// JavaScript Document
-
 function animateY(){
 	var element=document.getElementById("animate");
 	var currentPosition=0;
 	var id=setInterval(frame,5);
 	function frame(){
-		if(currentPosition==1500){
+		if(currentPosition==500){
 			clearInterval(id);
 			}
 		else{
 			currentPosition++;
 			element.style.top=currentPosition+'px';
-			
+
 			}
 		}
 	}
-	
