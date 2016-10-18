bbd4c140cab5a9a70f6f2031f39a641dbbf5a10e
karthikalaxmi <karthikapurnima9@gmail.com>
2016-10-18 12:02:40 +0530

    add counter decrements to given interva #15

--- src/DOM/counter_decrem/counter_decrem.js
+++ src/DOM/counter_decrem/counter_decrem.js
@@ -9,9 +9,9 @@ function timer()
   count=count-1;
   if (count <= 0)
   {
-     clearInterval(counter);
-     return;
+    clearInterval(counter);
+    return;
   }
 
- document.getElementById("timer").innerHTML=count + " secs"; // watch for spelling
+  document.getElementById("timer").innerHTML=count + " secs"; // watch for spelling
 }
