9629d8494bdd018a1813d7eb012314061dbaa584
karthikalaxmi <karthikapurnima9@gmail.com>
2016-10-19 10:36:56 +0530

    compute sphereArea  #4

--- src/functions/sphereArea/sphere.js
+++ src/functions/sphereArea/sphere.js
@@ -3,9 +3,8 @@ var sphereArea= function(r){
 
 	const pi=3.14;
 	area=4*pi*Math.pow(r,2);
-	  console.log(area);
-	  return area;
-
-  };
+	console.log(area);
+	return area;
+};
 
 module.exports = sphereArea;
--- src/functions/sphereArea/sphere.spec.js
+++ src/functions/sphereArea/sphere.spec.js
@@ -1,18 +1,16 @@
 // JavaScript Document
 var Code = require('code'),
-    expect = Code.expect,
-    sphereArea = require('./sphere.js');	
-	
+expect = Code.expect,
+sphereArea = require('./sphere.js');
+
 describe('computes sphere area' , function () {
-	
-	it('value of sphere_area'  , function () {
-		expect(sphereArea(3)).to.be.equal(113.04);
-	    expect(sphereArea(4)).to.be.equal(200.96);
-		expect(sphereArea(2)).to.be.equal(50.24);
-		expect(sphereArea(1)).to.be.equal(12.56);
-		
-    });
-		
+
+  it('value of sphere area'  , function () {
+     expect(sphereArea(3)).to.be.equal(113.04);
+     expect(sphereArea(4)).to.be.equal(200.96);
+     expect(sphereArea(2)).to.be.equal(50.24);
+     expect(sphereArea(1)).to.be.equal(12.56);
+
+   });
 
 })
-
