ec9e136667e8beeb87263d51acc691958afb7919
sumana0512 <sumana.alaparthi@gmail.com>
2016-10-20 10:45:09 +0530

    4 and 5 multiples in an array #1

---
+++ src/functions/multiples/multiples.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+function solution(number) {
+  var total = 0;
+  for (var i = 0; i <= number.length; i++)
+  {
+    if (number[i] % 4 === 0 || number[i] % 5 === 0)
+    total += number[i];
+  }
+  return total;
+};
+var total=solution([24,25,3]);
+console.log(total);
+
+module.exports = solution;
